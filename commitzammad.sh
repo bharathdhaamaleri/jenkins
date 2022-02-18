4554d5c0de5504da8909a3567019a2099a91d00d
Bharath Mani <bharath.mani@i-exceed.com>
2022-02-18 16:41:10 +0530

    syntax change for commit

--- commitzammad.sh
+++ commitzammad.sh
@@ -1,4 +1,4 @@
-if md5sum /var/lib/jenkins/workspace/"test projects"/"pipeline test"/sample*.war |grep -i "570f196c4a1025a717269d16d11d6f37"
+if md5sum /var/lib/jenkins/workspace/test\ projects/pipeline\ test/sample*.war |grep -i "570f196c4a1025a717269d16d11d6f37"
 then echo "passed"
 else echo "failed"
 fi
